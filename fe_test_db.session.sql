56cbc117d65c6bffc7f6c95ead90689e91821863
Vladimir <freshcode.yt@gmail.com>
2021-03-17 20:42:31 +0200

    Add relations between tables

--- fe_test_db.session.sql
+++ fe_test_db.session.sql
@@ -2,46 +2,26 @@ DROP TABLE "users";
 /*  */
 CREATE TABLE "users" (
   id serial PRIMARY KEY,
-  first_name varchar(64) NOT NULL CHECK (first_name != ''),
-  last_name varchar(64) NOT NULL CHECK (last_name != ''),
-  email varchar(256) NOT NULL UNIQUE,
+  first_name varchar(64) NOT NULL,
+  last_name varchar(64) NOT NULL,
+  email varchar(256) NOT NULL,
   is_male boolean NOT NULL,
-  birthday date NOT NULL CHECK (birthday < current_date),
+  birthday date NOT NULL CHECK (
+    birthday < current_date
+    AND birthday > '1900/1/1'
+  ),
   height numeric(3, 2) NOT NULL CHECK (
     height > 0.20
     AND height < 2.5
   ),
-  CONSTRAINT "CK_EMAIL_NOT_EMPTY" CHECK (email != '')
+  UNIQUE(first_name, last_name),
+  CONSTRAINT "CK_EMAIL_NOT_EMPTY" CHECK (email != ''),
+  CONSTRAINT "CK_FULL_NAME" CHECK (
+    first_name != ''
+    AND last_name != ''
+  )
 );
-
-
-
-
-
-
-/* Запретить повторение пар b, c */
-
-DROP TABLE A;
-
-CREATE TABLE A(
-  b int,
-  c int,
-  CONSTRAINT "UNIQUE_PAIR" PRIMARY KEY(b, c)
-);
-
-INSERT INTO A
-VALUES (1, 1),
-  (2, 1);
-
-
-
-
-
-
-
-
-
-
+/*  */
 INSERT INTO "users" (
     "first_name",
     "last_name",
@@ -52,19 +32,19 @@ INSERT INTO "users" (
   )
 VALUES (
     'Test',
-    'Testovich',
+    'Testovich1',
     'test1@mail.com',
     true,
     '1999-1-25',
-    0.5
+    0.6
   ),
   (
     'Test',
-    'Testovich',
+    'Testovich2',
     'hahahaha',
     true,
-    '1000-11-11',
-    0.30
+    '1900-11-11',
+    0.60
   ),
   (
     'Test',
@@ -76,24 +56,49 @@ VALUES (
   ),
   (
     '123',
-    'Testovich',
+    'Testovich4',
     'test3@mail.com',
     true,
     '1999-1-25',
     1.90
   );
-/*  */
-DROP TABLE "messages";
-/*  */
-CREATE TABLE "messages"(
-  "id" bigserial PRIMARY KEY,
-  "body" varchar(5000) NOT NULL CHECK("body" != ''),
-  "author" varchar(256) NOT NULL CHECK("author" != ''),
-  "createdAt" timestamp NOT NULL DEFAULT current_timestamp,
-  "isRead" boolean NOT NULL DEFAULT false
+CREATE TABLE "products"(
+  "id" serial PRIMARY KEY,
+  "name" varchar(256),
+  "category" varchar(128),
+  "price" decimal(16, 2) CHECK ("price" > 0),
+  "quantity" int CHECK("quantity" > 0),
+  UNIQUE("name", "category")
 );
+/* 
+ МНОГИЕ КО МНОГИМ
+ С ПОМОЩЬЮ СВЯЗУЮЩЕЙ ТАБЛИЦЫ
+ СОСТАВНОЙ ПЕРВИЧНЫЙ КЛЮЧ
+ */
+CREATE TABLE "products_to_orders"(
+  product_id integer,
+  order_id integer,
+  quantity integer,
+  PRIMARY KEY (product_id, order_id)
+);
+
+INSERT INTO "products_to_orders" ("product_id", "order_id", "quantity")
+VALUES (1,1,2),
+(3,1,1),
+(2,1,5);
+
 /*  */
-INSERT INTO "messages" ("author", "body")
-VALUES ('Test Testovich', 'test text'),
-  ('Test Testovich', 'test text'),
-  ('Test Testovich', 'test text');
+INSERT INTO "products" ("name", "category", "price", "quantity")
+VALUES ('sony xl', 'phone', 10000, 10000),
+  ('samsung XX', 'phone', 20000, 200),
+  ('acer', 'laptop', 30000, 20),
+  ('lenovo', 'laptop', 30000, 2000);
+/* СВЯЗЬ ОДИН к ОДНОМУ. REFERENCES */
+DROP TABLE "orders";
+CREATE TABLE "orders"(
+  "id" serial PRIMARY KEY,
+  "customer_id" int REFERENCES "users" ("id"),
+  "created_at" timestamp NOT NULL DEFAULT current_timestamp
+);
+INSERT INTO "orders" ("customer_id")
+VALUES(1);
