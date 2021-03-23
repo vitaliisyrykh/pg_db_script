78377fc349efaef659e7c8c0069814be7c70a567
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-03-23 16:52:08 +0200

    Add some tasks

--- fd_test_lesson.sql
+++ fd_test_lesson.sql
@@ -32,3 +32,37 @@ CREATE TABLE "users_to_orders"(
 SELECT sum(quantity)
 FROM phones_to_orders;
 /*  */
+
+SELECT sum("quantity") 
+FROM "phones";
+
+SELECT avg("price")FROM "phones";
+
+SELECT avg("price"), "brand"
+FROM "phones"
+GROUP BY "brand";
+
+SELECT sum("price"  * "quantity")
+FROM "phones"
+WHERE "price" BETWEEN 10000 AND 20000;
+
+SELECT count("model"), "brand"
+FROM "phones"
+GROUP BY "brand";
+
+SELECT * FROM "phones"
+ORDER BY "quantity" ASC;
+
+SELECT sum("quantity") AS "amount phones", "brand"
+FROM "phones"
+GROUP BY "brand"
+ORDER BY "amount phones" ASC;
+
+SELECT * FROM "users"
+ORDER BY extract('year'FROM age("birthday")) ASC, "firstName" ASC;
+
+SELECT sum("quantity"), "brand" 
+FROM "phones"
+GROUP BY "brand"
+HAVING sum("quantity") > 80000;
+
