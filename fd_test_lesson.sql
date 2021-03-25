b9501e4e68bce9f1a5ab4919d6d8b1c1e1ddf5e3
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-03-25 16:59:43 +0200

    add class tasks

--- fd_test_lesson.sql
+++ fd_test_lesson.sql
@@ -107,11 +107,11 @@ GROUP BY o.id;
 извлечь всех пользавтелей у которых кол-во заказов больше среднего
 */
 
-SELECT count(p.id), p.brand 
+SELECT  count(p.id), p.brand  
 FROM phones_to_orders AS pto
-  JOIN phones AS p ON p.id = pto."phoneId"
+ JOIN phones AS p ON p.id = pto."phoneId"
 WHERE pto."orderId" = 5  
-GROUP BY p.brand; 
+ GROUP BY p.brand ; 
 
 SELECT max(pto.quantity),o.id, sum(p.id)
 FROM phones_to_orders AS pto
@@ -119,7 +119,82 @@ FROM phones_to_orders AS pto
   JOIN phones AS p ON p.id = pto."phoneId"
 GROUP BY o.id,  p.id
 ORDER BY max(pto.quantity) ASC
-;  
+;
+
+/*кол-во заказов каждого пользвателей*/
+SELECT u.id, u.email, count(o.id)
+FROM users AS u
+  JOIN orders AS o ON u.id = o."userId"
+GROUP BY u.id
+ORDER BY u.id; 
+
+/*кол-во позиций товара в определённом заказе*/
+
+SELECT count(pto."phoneId"), o.id
+FROM phones_to_orders AS pto
+  JOIN orders AS o ON pto."orderId" = o.id
+WHERE o.id = 1
+GROUP BY o.id;
+
+/*извлечь самый популярный телефон*/
+
+SELECT p.brand, sum(pto.quantity)
+FROM phones AS p
+  JOIN phones_to_orders AS pto ON pto."phoneId" = p.id
+GROUP BY p.brand
+ORDER BY sum(pto.quantity) DESC
+LIMIT 1 ;
+
+
+/*извлечь пользавтелей и кол-во моделей, которые они покупали*/
+SELECT "uid_with_pid"."userId", count("uid_with_pid"."phoneId")
+FROM(
+  SELECT  u.id,u."firstName",count(pto."phoneId")
+  FROM users AS u
+    JOIN orders AS o ON o."userId" = u.id
+    JOIN phones_to_orders AS pto ON o.id = pto."orderId"
+    JOIN phones p ON p.id = pto."phoneId"
+  GROUP BY u.id
+)  AS "uid_with_pid"
+GROUP BY "uid_with_pid"."userId"
+
+/*извлечь всех пользавателем у которых кол-во заказов больше среднего*/
+
+WITH count_orders AS (SELECT u.id,count(o.id) AS "count", u.email 
+FROM "users" u
+  JOIN "orders" o ON u.id = o."userId"
+GROUP BY u.id)
+
+SELECT *
+FROM count_orders AS co
+WHERE co."count" > (
+  SELECT avg(co."count")
+  FROM count_orders AS co
+);
+
+CREATE TABLE "tasks"(
+  "id" serial PRIMARY KEY,
+  "heading" varchar(60) NOT NULL,
+  
+  "body" varchar(300) NOT NULL 
+);
+
+CREATE TABLE "tasks_to_users"(
+  "id" serial PRIMARY KEY,
+  "userId" int REFERENCES "users"("id"),
+  "taskId" int REFERENCES "tasks" ("id")
+);
+
+INSERT INTO "tasks"("heading", "body")
+VALUES ('some heading', 'some text body'),
+('some heading 2', 'some text body2'),
+('some heading 3', 'some text body3');
+
+INSERT INTO tasks_to_users ("userId", "taskId")
+VALUES (3,1),(3,2),(1,3);
+
+
+
 
 
 
