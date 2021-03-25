b386d6590f74410caeb649eb8662769c0fab7627
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-03-25 11:44:36 +0200

    ADD one task

--- fd_test_lesson.sql
+++ fd_test_lesson.sql
@@ -66,3 +66,75 @@ FROM "phones"
 GROUP BY "brand"
 HAVING sum("quantity") > 80000;
 
+
+SELECT char_length(concat("firstName", ' ', "lastName")) AS "fullNameLength", concat("lastName", ' ', "firstName") AS "fullName" 
+FROM "users"
+ORDER BY "fullName" DESC
+LIMIT 1;
+
+SELECT  char_length(concat("firstName", ' ', "lastName")) AS "length1", count(*) AS "amount"
+FROM "users"
+GROUP BY "length1"
+HAVING char_length(concat("firstName", ' ', "lastName")) < 18;
+
+SELECT char_length("email") AS "length", count(*)
+FROM "users"
+WHERE "email" LIKE 'm%'
+GROUP BY "length"
+HAVING char_length("email") >= 25
+ORDER BY "length";
+
+/* id заказов с айфонами*/
+SELECT o.id
+FROM orders AS o
+  JOIN phones_to_orders AS pto ON o.id = pto."orderId"
+  JOIN phones AS p ON pto."phoneId" = p.id
+WHERE p.brand ILIKE 'iphone';
+
+
+/* СТОИМОСТЬ КАЖДОГО ЗАКАЗА*/
+SELECT o.id, o."userId", sum(p.price * pto.quantity)
+FROM orders AS o
+  JOIN phones_to_orders AS pto ON pto."orderId" = o.id
+  JOIN phones AS p ON p.id = pto."phoneId"
+GROUP BY o.id;  
+
+
+/*извлечь все телефоны конкретного заказа
+извлечь самый популярный телефон
+извлечь пользавтелей и кол-во которые они покупали
+извлечь все заказы стомостьею выше среднего чека
+извлечь всех пользавтелей у которых кол-во заказов больше среднего
+*/
+
+SELECT count(p.id), p.brand 
+FROM phones_to_orders AS pto
+  JOIN phones AS p ON p.id = pto."phoneId"
+WHERE pto."orderId" = 5  
+GROUP BY p.brand; 
+
+SELECT max(pto.quantity),o.id, sum(p.id)
+FROM phones_to_orders AS pto
+  JOIN orders AS o ON o.id = pto."orderId"
+  JOIN phones AS p ON p.id = pto."phoneId"
+GROUP BY o.id,  p.id
+ORDER BY max(pto.quantity) ASC
+;  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
